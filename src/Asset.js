007be8f95800202165667d1d0d5e3d7d0dc5c731
Oddgeir Gitlestad <oddgeir.gitlestad@gmail.com>
2018-05-29 14:39:59 +0200

    Improved info form

---
+++ src/Asset.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import './App.css';
+
+const Asset = ({id}) => {
+  return (
+    <div className="row">
+      <div className="col-3">
+        <div className="form-group">
+          <label htmlFor="spouseName">Type</label>
+          <input type="text" className="form-control" id="spouseName" defaultValue="Kontanter" />
+        </div>
+      </div>
+      <div className="col-3">
+        <div className="form-group">
+          <label htmlFor="spouseName">Verdi</label>
+          <input type="text" className="form-control" id="spouseName" defaultValue="10 000 000,-" />
+        </div>
+      </div>
+      <div className="col-2">
+        <div>Eieform</div>
+        <div style={{ height: '16px' }} />
+        <div className="form-check">
+          <input type="checkbox" className="form-check-input" id={id} />
+          <label className="form-check-label" htmlFor={id}>Særeie</label>
+        </div>
+      </div>
+      <div className="col">
+      </div>
+    </div>
+  );
+};
+
+export default Asset;
--- src/InfoForm.js
+++ src/InfoForm.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import './App.css';
 import InfoBox from './InfoBox';
+import Asset from './Asset';
 
 const barn = () => (
   <div className="row">
@@ -24,11 +25,13 @@ class InfoForm extends Component {
     this.state = {
       spouse: false,
       children: 0,
-      weirdOwn: false
+      weirdOwn: false,
+      numOfAssets: 1,
     };
     this.onSpouseClick = this.onSpouseClick.bind(this);
     this.onChildrenChange = this.onChildrenChange.bind(this);
     this.onWeirdOwnClick = this.onWeirdOwnClick.bind(this);
+    this.onAddAssetClick = this.onAddAssetClick.bind(this);
   }
 
   onSpouseClick(ev) {
@@ -55,11 +58,23 @@ class InfoForm extends Component {
     )
   }
 
+  onAddAssetClick() {
+    this.setState({
+      ...this.state,
+      numOfAssets: this.state.numOfAssets + 1
+    })
+  }
+
   render() {
 
     const children = [];
     for (let i = 0; i < this.state.children; i++) {
       children.push(barn());
+    }
+
+    const assets = [];
+    for (let i = 0; i < this.state.numOfAssets; i++) {
+      assets.push(<Asset id={i} key={i}/>);
     }
 
     return (
@@ -87,38 +102,6 @@ class InfoForm extends Component {
             </div>
           </div>
           <div className="row">
-            <div className="col">
-              <h4>Eiendeler</h4>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-4">
-              <div className="form-group">
-                <label htmlFor="spouseName">Type</label>
-                <input type="text" className="form-control" id="spouseName" value="Kontanter" />
-              </div>
-            </div>
-            <div className="col-4">
-              <div className="form-group">
-                <label htmlFor="spouseName">Verdi</label>
-                <input type="text" className="form-control" id="spouseName" value="10 000 000,-" />
-              </div>
-            </div>
-            <div className="col">
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <button className="btn btn-default">Legg til eiendel</button>
-              <div style={{ height: '16px' }} />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <h3>Ektefelle</h3>
-            </div>
-          </div>
-          <div className="row">
             <div className="col-8">
               <div className="form-check">
                 <input onChange={this.onSpouseClick} type="checkbox" className="form-check-input" id="spouseCheck"/>
@@ -128,83 +111,6 @@ class InfoForm extends Component {
             <div className="col">
               <InfoBox title="Ektefelle"
                        body="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."/>
-            </div>
-          </div>
-          { this.state.spouse &&
-          <div>
-            <div className="row">
-              <div className="col-8">
-                <div className="form-group">
-                  <label htmlFor="spouseName">Navn</label>
-                  <input type="text" className="form-control" id="spouseName" placeholder="Navn"/>
-                </div>
-              </div>
-              <div className="col">
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-8">
-                <div className="form-check">
-                  <input onChange={this.onWeirdOwnClick} type="checkbox" className="form-check-input" id="spouseCheck"/>
-                  <label className="form-check-label" htmlFor="spouseCheck">Har dere særeie?</label>
-                </div>
-              </div>
-              <div className="col">
-                <InfoBox title="Særeie"
-                         body="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."/>
-              </div>
-            </div>
-            { this.state.weirdOwn &&
-            <div>
-              <div className="row">
-                <div className="col">
-                  <h4>Arvelaters del i særeie</h4>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-4">
-                  <div className="form-group">
-                    <label htmlFor="spouseName">Type</label>
-                    <input type="text" className="form-control" id="spouseName" value="Kontanter" />
-                  </div>
-                </div>
-                <div className="col-4">
-                  <div className="form-group">
-                    <label htmlFor="spouseName">Verdi</label>
-                    <input type="text" className="form-control" id="spouseName" value="2 000 000,-" />
-                  </div>
-                </div>
-                <div className="col">
-                </div>
-              </div>
-              <div className="row">
-                <div className="col">
-                  <h4>Ektefellens del i særeie</h4>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-4">
-                  <div className="form-group">
-                    <label htmlFor="spouseName">Type</label>
-                    <input type="text" className="form-control" id="spouseName" value="Kontanter" />
-                  </div>
-                </div>
-                <div className="col-4">
-                  <div className="form-group">
-                    <label htmlFor="spouseName">Verdi</label>
-                    <input type="text" className="form-control" id="spouseName" value="1 000 000,-" />
-                  </div>
-                </div>
-                <div className="col">
-                </div>
-              </div>
-            </div>
-            }
-          </div>
-          }
-          <div className="row">
-            <div className="col">
-              <h3>Barn</h3>
             </div>
           </div>
           <div className="row">
@@ -218,7 +124,47 @@ class InfoForm extends Component {
               <InfoBox title="Barn" body="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."/>
             </div>
           </div>
-          {children}
+          <div className="row">
+            <div className="col">
+              <h4>Eiendeler</h4>
+            </div>
+          </div>
+          {assets}
+          <div className="row">
+            <div className="col">
+              <button type="button" onClick={this.onAddAssetClick} className="btn btn-default">Legg til eiendel</button>
+              <div style={{ height: '16px' }} />
+            </div>
+          </div>
+          { this.state.spouse &&
+          <div>
+            <div className="row">
+              <div className="col">
+                <h3>Ektefelle</h3>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-8">
+                <div className="form-group">
+                  <label htmlFor="spouseName">Navn</label>
+                  <input type="text" className="form-control" id="spouseName" placeholder="Navn"/>
+                </div>
+              </div>
+              <div className="col">
+              </div>
+            </div>
+          </div>
+          }
+          {this.state.children > 0 &&
+          <div>
+            <div className="row">
+              <div className="col">
+                <h3>Barn</h3>
+              </div>
+            </div>
+            {children}
+          </div>
+          }
         </form>
         <Link to="/"><button className="btn btn-default">Tilbake</button></Link>
         <div style={{ display: 'inline-block', width: '8px' }} />
